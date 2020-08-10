55058583ad82b3a9ebe52263a441c9ca0b1656e2
CactusKipic <clement.azan@gmail.com>
2020-08-10 21:28:42 +0200

    Initial commit

--- src/main/java/fr/cactus_industries/Main.java
+++ src/main/java/fr/cactus_industries/Main.java
@@ -3,12 +3,16 @@ package fr.cactus_industries;
 import fr.cactus_industries.listeners.MessageListener;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
 
 public class Main {
     
     public static void main(String[] args0){
         String token = "";
-        
+        Yaml yaml = new Yaml();
+        File f = new File("./config.yml");
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
         api.addMessageCreateListener(new MessageListener("!"));
         
